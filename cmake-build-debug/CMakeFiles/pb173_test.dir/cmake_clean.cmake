434559d45ec062282e77c21f58edd63cec41c168
Roman <xhajduk@aisa.fi.muni.cz>
2017-02-20 13:28:01 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/pb173_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pb173_test.dir/main.cpp.o"
+  "pb173_test.pdb"
+  "pb173_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pb173_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
